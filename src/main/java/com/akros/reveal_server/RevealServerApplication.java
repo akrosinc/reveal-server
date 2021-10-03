f723e144d15fdb907411b19e94addcbff51c2b0e
Linda Oscar Gcaba <lindagcaba@gmail.com>
2021-10-03 12:57:52 +0200

    boot setup

---
+++ src/main/java/com/akros/reveal_server/RevealServerApplication.java
@@ -0,0 +1,13 @@
+package com.akros.reveal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevealServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevealServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akros/reveal_server/RevealServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akros.reveal_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevealServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
