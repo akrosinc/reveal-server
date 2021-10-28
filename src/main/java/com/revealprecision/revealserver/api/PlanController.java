0e35fe1c09641755ba924b3af8c20149dbfba875
Linda Oscar Gcaba <lindagcaba@gmail.com>
2021-10-28 22:06:20 +0200

    skeleton spring boot with embedded undertow running, wired sample controllers and service to models.

--- src/main/java/com/revealprecision/revealserver/api/PlanController.java
+++ src/main/java/com/revealprecision/revealserver/api/PlanController.java
@@ -1,50 +1,32 @@
-package com.revealprecision.revealserver.api.controller;
+package com.revealprecision.revealserver.api;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import com.revealprecision.revealserver.persistence.domain.Plan;
 import com.revealprecision.revealserver.service.PlanService;
-
 import io.swagger.v3.oas.annotations.Operation;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/")
 public class PlanController {
+	private PlanService planService;
 
-	@Operation(summary = "Search for plan definitions",
-		description = "Search for plan definitions using path variables as filters",
-		tags = { "Plan" }
+	@Autowired
+	public PlanController(PlanService planService) {
+		this.planService = planService;
+	}
+	@Operation(summary = "Fetch All Plans",
+			description = "Fetch All Plans",
+			tags = { "Plan" }
 	)
 	@GetMapping(value = "/plan",
-		produces = "application/json"
+			produces = "application/json"
 	)
-	public List<PlanResponse> searchPlans (
-		// @Parameter(description = "Search by plan title")
-		// @Pattern(regex = "[A-Za-z0-9\\-]*")
-		// @Size(min = 3, max = 20, message = "title parameter length must be between 3 and 20 characters")
-		@RequestParam(value = "title", required = false) String planTitle,
-
-		// @Parameter(description = "Search by plan status")
-		// @RequestParam(value = "status", required = false, defaultValue = "Active") Enum PlanStatusEnum,
-
-		// @Parameter(description = "Search by plan interventionType")
-		// @RequestParam(value = "interventionType", required = false) Enum PlanInterventionTypeEnum,
-
-		// @Parameter(description = "Search by plan jurisdiction")
-		// @RequestParam(value = "jurisdiction", required = false) String planJurisdiction,
-		// @Parameter(description = "filter by pageNumber")
-		@RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
-
-		// @Parameter(description = "filter by pageSize")
-		@RequestParam(name = "pageSize", required = false, defaultValue = "10") int pageSize) {
-
-			return new PlanService.searchPlans(planTitle, PageRequest.of(pageNumber, pageSize));
+	public List<Plan> getPlans(){
+		return planService.getPlans();
 	}
 }
