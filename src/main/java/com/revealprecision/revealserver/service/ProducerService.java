780e19f1e08375e0ec101107c1952ac40c220fc1
Fanie Heath <faneroo@gmail.com>
2021-11-09 09:15:12 +0200

    updates to code

--- src/main/java/com/revealprecision/revealserver/service/ProducerService.java
+++ src/main/java/com/revealprecision/revealserver/service/ProducerService.java
@@ -1,2 +1,40 @@
-package com.revealprecision.revealserver.service;public class ProducerService {
+package com.revealprecision.revealserver.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Service
+public class ProducerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProducerService.class);
+    private static final String KAFKA_TOPIC = "reveal";
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public ProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+
+        ListenableFuture<SendResult<String, String>> future =
+                kafkaTemplate.send(KAFKA_TOPIC, message);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                logger.info(String.format("Sent message to Kafka on topic %s at offset %s", result.getRecordMetadata().topic(), result.getRecordMetadata().offset()));
+            }
+            @Override
+            public void onFailure(Throwable ex) {
+                logger.info(String.format("Unable to send message to Kafka due to %s", ex.getMessage()));
+            }
+        });
+    }
 }
