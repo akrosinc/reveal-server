ef41cf464b81f82d938d36d8da9483560ad9f3a2
Fanie Heath <faneroo@gmail.com>
2021-11-19 14:42:29 +0200

    pushing changes to task

--- src/main/java/com/revealprecision/revealserver/enums/GenderEnum.java
+++ src/main/java/com/revealprecision/revealserver/enums/GenderEnum.java
@@ -8,7 +8,7 @@ public enum GenderEnum {
     male,
     female,
     other,
-    Unknown
+    unknown
 }
 
 
---
+++ src/main/java/com/revealprecision/revealserver/service/TaskService.java
@@ -0,0 +1,49 @@
+package com.revealprecision.revealserver.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revealprecision.revealserver.persistence.domain.Task;
+import com.revealprecision.revealserver.persistence.repository.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    private static final Logger logger = LoggerFactory.getLogger(TaskService.class);
+    private final TaskRepository taskRepository;
+    private final ProducerService producerService;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository, ProducerService producerService, ObjectMapper objectMapper) {
+        this.taskRepository = taskRepository;
+        this.producerService = producerService;
+        this.objectMapper = objectMapper;
+    }
+    public Page<Task> getTasks(Integer pageNumber, Integer pageSize){
+        return taskRepository.findAll(PageRequest.of(pageNumber,pageSize));
+    }
+
+    public Task createTask(Task task) {
+        Task save = taskRepository.save(task);
+        logger.info("Task saved to database as {}", task);
+
+//        try {
+//            producerService.sendMessage(objectMapper.writeValueAsString(task));
+//        } catch (JsonProcessingException e) {
+//            logger.debug("Task not mapped {}", e.getMessage());
+//        }
+        return save;
+    }
+
+    public Task getTaskByIdentifier(String taskIdentifier) {
+        return taskRepository.findByIdentifier(taskIdentifier);
+    }
+
+    public Task updateTask(String identifier, Task task) {
+        return null;
+    }
+}
