d6ab446df6ab561c8c5381e1ccd937536454d0ba
Linda Oscar Gcaba <lindagcaba@gmail.com>
2021-11-20 13:56:24 +0200

    remove unused

--- src/main/java/com/revealprecision/revealserver/persistence/domain/Task.java
+++ src/main/java/com/revealprecision/revealserver/persistence/domain/Task.java
@@ -7,7 +7,6 @@ import lombok.Setter;
 import org.hibernate.envers.Audited;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.Date;
 
