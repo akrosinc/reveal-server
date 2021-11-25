41c99f56e328c239576308e55075121ab1ad700f
Fanie Heath <faneroo@gmail.com>
2021-11-25 16:47:54 +0200

    fixing case of the constants of enums

--- src/main/java/com/revealprecision/revealserver/enums/BusinessStatusEnum.java
+++ src/main/java/com/revealprecision/revealserver/enums/BusinessStatusEnum.java
@@ -1,12 +1,12 @@
 package com.revealprecision.revealserver.enums;
 
 public enum BusinessStatusEnum {
-    not_visited,
-    not_eligible,
-    not_sprayed,
-    ineligible,
-    complete,
-    not_dispensed,
-    dispense_complete,
-    adherence_complete
+    NOT_VISITED,
+    NOT_ELIGIBLE,
+    NOT_SPRAYED,
+    INELIGIBLE,
+    COMPLETE,
+    NOT_DISPENSED,
+    DISPENSE_COMPLETE,
+    ADHERENCE_COMPLETE
 }
