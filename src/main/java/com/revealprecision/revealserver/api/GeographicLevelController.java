a62d4d5490f06ef9113b55fabdff55b46571b931
Fanie Heath <faneroo@gmail.com>
2021-11-26 12:55:41 +0200

    pushing some annotations to the controllers

--- src/main/java/com/revealprecision/revealserver/api/GeographicLevelController.java
+++ src/main/java/com/revealprecision/revealserver/api/GeographicLevelController.java
@@ -3,6 +3,7 @@ package com.revealprecision.revealserver.api;
 import com.revealprecision.revealserver.persistence.domain.GeographicLevel;
 import com.revealprecision.revealserver.service.GeographicLevelService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -34,22 +35,39 @@ public class GeographicLevelController {
         return geographicLevelService.createGeographicLevel(geographicLevel);
     }
 
+    @Operation(summary = "List geographicLevels",
+            description = "List geographicLevels",
+            tags = { "Location" }
+    )
     @GetMapping(value = "/geographicLevel", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Page<GeographicLevel> getGeographicLevels(@RequestParam(defaultValue = "0") Integer pageNumber, @RequestParam(defaultValue = "50") Integer pageSize){
+    public Page<GeographicLevel> getGeographicLevels(
+            @Parameter(description = "Page number to return") @RequestParam(defaultValue = "0", required = false) Integer pageNumber,
+            @Parameter(description = "Number of records per page") @RequestParam(defaultValue = "50", required = false) Integer pageSize){
         return geographicLevelService.getGeographicLevels(pageNumber,pageSize);
     }
 
+    @Operation(summary = "Fetch a geographicLevel",
+            description = "Fetch a geographicLevel",
+            tags = { "Location" }
+    )
     @GetMapping(value = "/geographicLevel/{identifier}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public Object findGeographicLevelByIdentifier(@PathVariable UUID identifier){
-        Optional<GeographicLevel> levelOptional = geographicLevelService.findGeographicLevelByIdentifier(identifier);
-        if(levelOptional.isPresent()){
-            return levelOptional.get();
-        }
-        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    public Object findGeographicLevelByIdentifier(
+            @Parameter(description = "Identifier of the geographicLevel") @PathVariable UUID identifier){
+                Optional<GeographicLevel> levelOptional = geographicLevelService.findGeographicLevelByIdentifier(identifier);
+                if(levelOptional.isPresent()){
+                    return levelOptional.get();
+                }
+                return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Update a geographicLevel",
+            description = "Update a geographicLevel",
+            tags = { "Location" }
+    )
     @PutMapping(value = "/geographicLevel/{identifier}",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public GeographicLevel updateGeographicLevel(@RequestBody GeographicLevel geographicLevel,@PathVariable UUID identifier){
+    public GeographicLevel updateGeographicLevel(
+            @RequestBody GeographicLevel geographicLevel,
+            @Parameter(description = "Identifier of the geographicLevel") @PathVariable UUID identifier){
         return geographicLevelService.update(identifier,geographicLevel);
     }
 
