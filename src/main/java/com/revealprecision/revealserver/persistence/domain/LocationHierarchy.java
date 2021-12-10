dee391863153f5b796f4d777fa829e912fe3f2ce
Milan Katic <milan.katic@vega.local>
2021-12-10 09:07:35 +0100

    bug fixing entity_status

--- src/main/java/com/revealprecision/revealserver/persistence/domain/LocationHierarchy.java
+++ src/main/java/com/revealprecision/revealserver/persistence/domain/LocationHierarchy.java
@@ -1,25 +1,29 @@
 package com.revealprecision.revealserver.persistence.domain;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.hibernate.envers.Audited;
-
+import java.util.UUID;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
-import java.util.UUID;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.envers.Audited;
 
 @Entity
 @Audited
 @Getter
 @Setter
 @RequiredArgsConstructor
+@SQLDelete(sql = "UPDATE location_hierarchy SET entity_status = 'DELETED' where identifier=?")
+@Where(clause = "entity_status='ACTIVE'")
 public class LocationHierarchy extends AbstractAuditableEntity {
-    @Id
-    @GeneratedValue
-    private UUID identifier;
-    @NotBlank(message = "order is required and must not be empty")
-    private String node_order;
+
+  @Id
+  @GeneratedValue
+  private UUID identifier;
+  @NotBlank(message = "order is required and must not be empty")
+  private String node_order;
 }
--- src/main/java/com/revealprecision/revealserver/persistence/domain/Plan.java
+++ src/main/java/com/revealprecision/revealserver/persistence/domain/Plan.java
@@ -2,14 +2,21 @@ package com.revealprecision.revealserver.persistence.domain;
 
 import com.revealprecision.revealserver.enums.PlanInterventionTypeEnum;
 import com.revealprecision.revealserver.enums.PlanStatusEnum;
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.envers.Audited;
-
-import javax.persistence.*;
-import java.util.Date;
-import java.util.UUID;
 
 @Entity
 @Audited
@@ -17,20 +24,23 @@ import java.util.UUID;
 @Setter
 @RequiredArgsConstructor
 @NamedNativeQueries(
-        @NamedNativeQuery(name = "findByIdentifier",
-                          query = "select * from plan where identifier = ?",
-                          resultClass = Plan.class)
+    @NamedNativeQuery(name = "findByIdentifier",
+        query = "select * from plan where identifier = ? where entity_status='ACTIVE'",
+        resultClass = Plan.class)
 )
+@SQLDelete(sql = "UPDATE plan SET entity_status = 'DELETED' where identifier=?")
+@Where(clause = "entity_status='ACTIVE'")
 public class Plan extends AbstractAuditableEntity {
-    @Id
-    @GeneratedValue
-    private UUID identifier;
-    private String name;
-    private String title;
-    private Date effectivePeriodStart;
-    private Date effectivePeriodEnd;
-    @Enumerated(EnumType.STRING)
-    private PlanStatusEnum status;
-    @Enumerated(EnumType.STRING)
-    private PlanInterventionTypeEnum interventionType;
+
+  @Id
+  @GeneratedValue
+  private UUID identifier;
+  private String name;
+  private String title;
+  private Date effectivePeriodStart;
+  private Date effectivePeriodEnd;
+  @Enumerated(EnumType.STRING)
+  private PlanStatusEnum status;
+  @Enumerated(EnumType.STRING)
+  private PlanInterventionTypeEnum interventionType;
 }
