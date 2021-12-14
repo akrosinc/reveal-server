8c17938304e545affbbc38dd3241e7170d5d5560
Milan Katic <milan.katic@vega.local>
2021-12-14 15:00:36 +0100

    cors config for local env

--- src/main/java/com/revealprecision/revealserver/config/KeycloakSecurityConfig.java
+++ src/main/java/com/revealprecision/revealserver/config/KeycloakSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.revealprecision.revealserver.config;
 
+import java.util.List;
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
@@ -15,6 +16,7 @@ import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +27,19 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeRequests()
-                .anyRequest().permitAll();
+            .anyRequest().permitAll();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(
+            List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+        corsConfiguration.setAllowedMethods(
+            List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        http.cors().configurationSource(request -> corsConfiguration);
         http.csrf().disable();
+
+        //TODO update configuration for production env
     }
 
     @Autowired
