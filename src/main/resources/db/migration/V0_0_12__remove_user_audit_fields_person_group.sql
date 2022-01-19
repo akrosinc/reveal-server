b0204cc317f61e9445a7c980492efcbe1cb06fa7
trevlen bahadur <tbahadur@akros.com>
2022-01-19 05:57:29 +0200

    collapsing db scripts

--- src/main/resources/db/migration/V0_0_12__remove_user_audit_fields_person_group.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE IF EXISTS person_group DROP COLUMN IF EXISTS created_by;
-ALTER TABLE IF EXISTS person_group DROP COLUMN IF EXISTS created_datetime;
-ALTER TABLE IF EXISTS person_group DROP COLUMN IF EXISTS modified_by;
-ALTER TABLE IF EXISTS person_group DROP COLUMN IF EXISTS modified_datetime;
-
-ALTER TABLE IF EXISTS person_group_aud DROP COLUMN IF EXISTS created_by;
-ALTER TABLE IF EXISTS person_group_aud DROP COLUMN IF EXISTS created_datetime;
-ALTER TABLE IF EXISTS person_group_aud DROP COLUMN IF EXISTS modified_by;
-ALTER TABLE IF EXISTS person_group_aud DROP COLUMN IF EXISTS modified_datetime;
--- src/main/resources/db/migration/V0_0_7__alter_person_birthdate.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE IF EXISTS person
-    RENAME "birthDate" TO birth_date;
