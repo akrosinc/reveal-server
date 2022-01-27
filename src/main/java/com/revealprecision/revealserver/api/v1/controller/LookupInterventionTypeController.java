adc96a46ffb00aa573a97347666dbdd3cc14b815
Milan Katic <milan.katic@vegait.rs>
2022-01-27 10:18:19 +0100

    added api for getting intervention types

--- src/main/java/com/revealprecision/revealserver/api/v1/controller/LookupInterventionTypeController.java
+++ src/main/java/com/revealprecision/revealserver/api/v1/controller/LookupInterventionTypeController.java
@@ -1,11 +1,15 @@
 package com.revealprecision.revealserver.api.v1.controller;
 
+import com.revealprecision.revealserver.api.v1.dto.factory.LookupInterventionTypeResponseFactory;
 import com.revealprecision.revealserver.api.v1.dto.request.LookupInterventionTypeRequest;
+import com.revealprecision.revealserver.api.v1.dto.response.LookupInterventionTypeResponse;
 import com.revealprecision.revealserver.service.LookupInterventionTypeService;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +27,13 @@ public class LookupInterventionTypeController {
       LookupInterventionTypeRequest request) {
     interventionTypeService.createInterventionType(request);
     return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @GetMapping
+  public ResponseEntity<List<LookupInterventionTypeResponse>> findAll() {
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(LookupInterventionTypeResponseFactory.fromEntityList(
+            interventionTypeService.findAll()));
   }
 }
