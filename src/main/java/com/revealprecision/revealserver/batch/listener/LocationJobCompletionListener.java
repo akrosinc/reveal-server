2f41c1ce4ce0960f659621676103b272f6fa3b6a
Milan Katic <milan.katic@vegait.rs>
2022-02-07 13:21:35 +0100

    fixing generating location relationships

--- src/main/java/com/revealprecision/revealserver/batch/listener/LocationJobCompletionListener.java
+++ src/main/java/com/revealprecision/revealserver/batch/listener/LocationJobCompletionListener.java
@@ -1,11 +1,14 @@
 package com.revealprecision.revealserver.batch.listener;
 
 import com.revealprecision.revealserver.enums.BulkStatusEnum;
+import com.revealprecision.revealserver.persistence.domain.Location;
 import com.revealprecision.revealserver.persistence.domain.LocationBulk;
 import com.revealprecision.revealserver.persistence.repository.LocationBulkRepository;
 import com.revealprecision.revealserver.service.LocationBulkService;
+import com.revealprecision.revealserver.service.LocationRelationshipService;
 import com.revealprecision.revealserver.service.StorageService;
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.JobExecution;
@@ -19,6 +22,7 @@ public class LocationJobCompletionListener implements JobExecutionListener {
   private final LocationBulkService locationBulkService;
   private final LocationBulkRepository locationBulkRepository;
   private final StorageService storageService;
+  private final LocationRelationshipService locationRelationshipService;
 
   @Override
   public void beforeJob(JobExecution jobExecution) {
@@ -30,8 +34,15 @@ public class LocationJobCompletionListener implements JobExecutionListener {
     String locationBulkId = jobExecution.getJobParameters().getString("locationBulkId");
     String filePath = jobExecution.getJobParameters().getString("filePath");
     LocationBulk locationBulk = locationBulkService.findById(UUID.fromString(locationBulkId));
+
+    List<Location> addedLocations = locationBulkRepository.getAllCreatedInBulk(
+        locationBulk.getIdentifier());
+    addedLocations.forEach(
+        locationRelationshipService::createRelationshipForImportedLocation);
+
     locationBulk.setStatus(BulkStatusEnum.COMPLETE);
     locationBulkRepository.save(locationBulk);
+
     try {
       storageService.deleteFile(filePath);
     } catch (IOException e) {
