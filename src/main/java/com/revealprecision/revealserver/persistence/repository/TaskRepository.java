de7efcbf682b807b418ba5934314cb925b283c30
trevlen bahadur <tbahadur@akros.com>
2022-02-25 13:52:11 +0200

    Task Android OpenSRP Facade

--- src/main/java/com/revealprecision/revealserver/persistence/repository/TaskRepository.java
+++ src/main/java/com/revealprecision/revealserver/persistence/repository/TaskRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
