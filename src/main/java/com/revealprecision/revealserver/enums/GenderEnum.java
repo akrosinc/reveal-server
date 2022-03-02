6d07cf8aa1dfe5eb02e42583f163023b1b8e54b5
trevlen bahadur <tbahadur@akros.com>
2022-03-02 16:24:30 +0200

    Using the lookup util for enum checks

--- src/main/java/com/revealprecision/revealserver/enums/GenderEnum.java
+++ src/main/java/com/revealprecision/revealserver/enums/GenderEnum.java
@@ -1,37 +1,10 @@
 package com.revealprecision.revealserver.enums;
 
-import java.util.HashMap;
-import java.util.Map;
 import lombok.Getter;
 
-/*
-Genders are introduced as defined by https://www.hl7.org/fhir/person.html
-*/
 @Getter
 public enum GenderEnum {
-  MALE("male"),
-  FEMALE("female"),
-  OTHER("other"),
-  UNKNOWN("unknown");
-
-  private final String value;
-
-  GenderEnum(String value) {
-    this.value = value;
-  }
-
-  private static final Map<String, GenderEnum> BY_VALUE = new HashMap<>();
-
-  static {
-    for (GenderEnum e : values()) {
-      BY_VALUE.put(e.value, e);
-    }
-  }
-
-  public static GenderEnum getEnum(String value) {
-    return BY_VALUE.get(value);
-  }
-
+  MALE, FEMALE, OTHER, UNKNOWN
 }
 
 
--- src/main/java/com/revealprecision/revealserver/enums/GroupTypeEnum.java
+++ src/main/java/com/revealprecision/revealserver/enums/GroupTypeEnum.java
@@ -1,30 +1,9 @@
 package com.revealprecision.revealserver.enums;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.Getter;
 
 @Getter
 public enum GroupTypeEnum implements Serializable {
-  FAMILY("family");
-
-
-  private final String value;
-
-  GroupTypeEnum(String value) {
-    this.value = value;
-  }
-
-  private static final Map<String, GroupTypeEnum> BY_VALUE = new HashMap<>();
-
-  static {
-    for (GroupTypeEnum e : values()) {
-      BY_VALUE.put(e.value, e);
-    }
-  }
-
-  public static GroupTypeEnum getEnum(String value) {
-    return BY_VALUE.get(value);
-  }
+  FAMILY
 }
