c3fb47aac4322ae27dec57386e45a02f18ad08dc
Linda Oscar Gcaba <lindagcaba@gmail.com>
2022-03-02 10:55:59 +0200

    added location add endpoint for android.

--- src/main/java/com/revealprecision/revealserver/api/v1/facade/controller/LocationFacadeController.java
+++ src/main/java/com/revealprecision/revealserver/api/v1/facade/controller/LocationFacadeController.java
@@ -9,6 +9,7 @@ import com.revealprecision.revealserver.persistence.domain.LocationHierarchy;
 import com.revealprecision.revealserver.service.LocationHierarchyService;
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -18,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,8 +29,13 @@ public class LocationFacadeController {
 
   private final LocationFacadeService locationFacadeService;
   private final LocationHierarchyService locationHierarchyService;
+  public static final String IS_JURISDICTION = "is_jurisdiction";
+  private static final String FALSE = "false";
 
-  @Operation(summary = "Sync Locations for Android app", description = "Sync Locations for Android app", tags = {
+
+
+
+  @Operation(summary = "Sync Locations for Reveal mobile app", description = "Sync Locations for Reveal mobile app", tags = {
       "Location Sync Facade"})
   @PostMapping(value = "/sync", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<List<PhysicalLocation>> getLocations(
@@ -51,5 +58,13 @@ public class LocationFacadeController {
     return ResponseEntity.status(HttpStatus.OK).headers(headers).body(physicalLocations);
   }
 
+  @Operation(summary = "Update server with structure/Location created from reveal mobile app", description = "Update server with structure/Location created from reveal mobile app", tags = {
+      "Location Sync Facade"})
+  @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<String> addSyncedStructures(@RequestBody List<PhysicalLocation> physicalLocations, @RequestParam(value = IS_JURISDICTION, defaultValue = FALSE, required = false) boolean isJurisdiction) {
+   Set<String> locationRequestsWithErrors = locationFacadeService.saveSyncedLocations(physicalLocations);
+   String message =  locationRequestsWithErrors.isEmpty() ?  "All Locations  processed" :  ("Locations with Ids not processed: " + String.join(",", locationRequestsWithErrors));
+    return ResponseEntity.status(HttpStatus.CREATED).body( message);
+  }
 
 }
--- src/main/java/com/revealprecision/revealserver/api/v1/facade/service/LocationFacadeService.java
+++ src/main/java/com/revealprecision/revealserver/api/v1/facade/service/LocationFacadeService.java
@@ -1,18 +1,26 @@
 package com.revealprecision.revealserver.api.v1.facade.service;
 
+import com.revealprecision.revealserver.api.v1.dto.request.LocationRequest;
+import com.revealprecision.revealserver.api.v1.facade.factory.LocationRequestFactory;
+import com.revealprecision.revealserver.api.v1.facade.models.PhysicalLocation;
 import com.revealprecision.revealserver.api.v1.facade.request.LocationSyncRequest;
 import com.revealprecision.revealserver.persistence.domain.Location;
 import com.revealprecision.revealserver.persistence.domain.LocationHierarchy;
 import com.revealprecision.revealserver.service.LocationService;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class LocationFacadeService {
 
   public static final String TOTAL_RECORDS = "total_records";
@@ -51,6 +59,20 @@ public class LocationFacadeService {
       HttpHeaders headers) {
     headers.add(TOTAL_RECORDS, String.valueOf(count));
     return headers;
+  }
+
+  public Set<String> saveSyncedLocations(List<PhysicalLocation> physicalLocationRequests){
+    Set<String> locationRequestsWithErrors = new HashSet<>();
+    List<LocationRequest> locationRequests = LocationRequestFactory.fromPhysicalLocationRequests(physicalLocationRequests);
+    for(LocationRequest locationRequest:locationRequests){
+        try {
+          locationService.createLocation(locationRequest);
+        }catch (Exception e){
+          log.error(e.getMessage(), e);
+          locationRequestsWithErrors.add(locationRequest.getProperties().getExternalId().toString());
+        }
+    }
+    return  locationRequestsWithErrors;
   }
 
 }
