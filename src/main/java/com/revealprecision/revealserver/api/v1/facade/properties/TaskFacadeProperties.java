d293d7695b67df487f55ed09b02ae142457d016d
trevlen bahadur <tbahadur@akros.com>
2022-03-02 12:34:58 +0200

    fixing the Class naming

--- src/main/java/com/revealprecision/revealserver/api/v1/facade/properties/TaskFacadeProperties.java
+++ src/main/java/com/revealprecision/revealserver/api/v1/facade/properties/TaskFacadeProperties.java
@@ -1,9 +1,7 @@
 package com.revealprecision.revealserver.api.v1.facade.properties;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import javax.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -12,7 +10,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "task.facade")
 @Component
 @Setter @Getter
-public class TasKFacadeProperties {
+public class TaskFacadeProperties {
 
   Map<String,String> businessStatusMapping = Collections.singletonMap("GENERAL","business_status");
 
