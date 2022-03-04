55c9af917e604d8d4f948b858c9aa8f0e294738d
Linda Oscar Gcaba <lindagcaba@gmail.com>
2022-03-04 10:39:04 +0200

    add some documentation

--- src/main/java/com/revealprecision/revealserver/api/v1/controller/SettingsController.java
+++ src/main/java/com/revealprecision/revealserver/api/v1/controller/SettingsController.java
@@ -6,6 +6,7 @@ import com.revealprecision.revealserver.api.v1.dto.request.SettingRequest;
 import com.revealprecision.revealserver.api.v1.dto.response.SettingConfigurationResponse;
 import com.revealprecision.revealserver.api.v1.dto.response.SettingResponse;
 import com.revealprecision.revealserver.api.v1.facade.service.SettingService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -28,18 +29,27 @@ public class SettingsController {
 
   private final SettingService settingService;
 
+  @Operation(summary = "Fetch Settings Configuration by specifying settingsIdentifier", description = "Fetch Settings Configuration by specifying settingsTypeIdentifier, to support mobile application the search parameter is call identifier (don't be confused)", tags = {
+      "Setting"})
   @GetMapping(value = "/sync", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<SettingConfigurationResponse> getSettings(@Parameter(description = "used to filter the type of configs to fetch") @RequestParam(name = "identifier", defaultValue = "global_configs") String identifier,
-     @Parameter(description = "Required by previous implementation, currently not used") @RequestParam(name = "serverVersion") String serverVersion) {
-    return ResponseEntity.status(HttpStatus.OK).body(SettingConfigurationResponseFactory.fromSettingsAndSettingTypeIdentifier(settingService.findExistingSettingsByTypeIdentifier(identifier),identifier));
+  public ResponseEntity<SettingConfigurationResponse> getSettings(
+      @Parameter(description = "used to filter the type of configs to fetch") @RequestParam(name = "identifier", defaultValue = "global_configs") String identifier,
+      @Parameter(description = "Required by previous implementation, currently not used") @RequestParam(name = "serverVersion") String serverVersion) {
+    return ResponseEntity.status(HttpStatus.OK).body(SettingConfigurationResponseFactory
+        .fromSettingsAndSettingTypeIdentifier(
+            settingService.findExistingSettingsByTypeIdentifier(identifier), identifier));
   }
 
+  @Operation(summary = "Create a setting by supplying key and other required details", description = "Create a setting by supplying key and other required details", tags = {
+      "Setting"})
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<SettingResponse> createSetting(@RequestBody SettingRequest settingRequest) {
     return ResponseEntity.status(HttpStatus.OK)
         .body(SettingResponseFactory.fromEntity(settingService.createSetting(settingRequest)));
   }
 
+  @Operation(summary = "Update setting values", description = "Update setting values", tags = {
+      "Settings"})
   @PutMapping(value = "{identifier}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<SettingResponse> updateSetting(@PathVariable UUID identifier,
       @RequestBody SettingRequest settingRequest) {
