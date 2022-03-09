f3c2d30d77181c92d2d491dc94165a276c735f3d
Linda Oscar Gcaba <lindagcaba@gmail.com>
2022-03-09 11:26:09 +0200

    location properties  fix.

--- src/main/java/com/revealprecision/revealserver/persistence/domain/LocationProperty.java
+++ src/main/java/com/revealprecision/revealserver/persistence/domain/LocationProperty.java
@@ -3,12 +3,16 @@ package com.revealprecision.revealserver.persistence.domain;
 import java.util.UUID;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class LocationProperty {
 
