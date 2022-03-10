3e7a5e20ee3904de77499bb53afa4a71a8309b6e
trevlenb2 <trevlenb2@gmail.com>
2022-03-10 16:29:04 +0200

    refactor

--- src/main/java/com/revealprecision/revealserver/fhir/providers/PlanResourceProvider.java
+++ src/main/java/com/revealprecision/revealserver/fhir/providers/PlanResourceProvider.java
@@ -3,13 +3,10 @@ package com.revealprecision.revealserver.fhir.providers;
 import ca.uhn.fhir.rest.annotation.IdParam;
 import ca.uhn.fhir.rest.annotation.Read;
 import ca.uhn.fhir.rest.server.IResourceProvider;
-import com.revealprecision.revealserver.fhir.util.FhirActionUtil;
+import com.revealprecision.revealserver.fhir.util.FhirPlanUtil;
 import com.revealprecision.revealserver.persistence.domain.Goal;
-import com.revealprecision.revealserver.persistence.domain.Location;
 import com.revealprecision.revealserver.persistence.domain.Plan;
-import com.revealprecision.revealserver.persistence.domain.Target;
 import com.revealprecision.revealserver.service.PlanService;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -18,19 +15,12 @@ import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
-import org.hl7.fhir.r4.model.DecimalType;
-import org.hl7.fhir.r4.model.Duration;
 import org.hl7.fhir.r4.model.Enumerations.PublicationStatus;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.PlanDefinition;
-import org.hl7.fhir.r4.model.PlanDefinition.PlanDefinitionGoalComponent;
-import org.hl7.fhir.r4.model.PlanDefinition.PlanDefinitionGoalTargetComponent;
-import org.hl7.fhir.r4.model.Quantity;
 import org.hl7.fhir.r4.model.Reference;
-import org.hl7.fhir.r4.model.StringType;
 import org.hl7.fhir.r4.model.UsageContext;
 import org.springframework.stereotype.Component;
 
@@ -58,9 +48,9 @@ public class PlanResourceProvider implements IResourceProvider {
 
     Set<Goal> revealGoals = plan.getGoals();
     revealGoals.stream().flatMap(goals -> goals.getActions().stream())
-        .map(FhirActionUtil::getActionComponent).forEach(planDefinition::addAction);
+        .map(FhirPlanUtil::getActionComponent).forEach(planDefinition::addAction);
 
-    revealGoals.stream().map(this::getPlanDefinitionGoalComponent).forEach(planDefinition::addGoal);
+    revealGoals.stream().map(FhirPlanUtil::getPlanDefinitionGoalComponent).forEach(planDefinition::addGoal);
 
     planDefinition.setStatus(PublicationStatus.fromCode(plan.getStatus().toString().toLowerCase()));
 
@@ -80,64 +70,10 @@ public class PlanResourceProvider implements IResourceProvider {
     useContexts.add(useContext);
     planDefinition.setUseContext(useContexts);
 
-    planService.findLocationsForPlan(plan.getIdentifier()).stream().map(this::getPlanJurisdictions)
+    planService.findLocationsForPlan(plan.getIdentifier()).stream().map(FhirPlanUtil::getPlanJurisdictions)
         .forEach(planDefinition::addJurisdiction);
     return planDefinition;
   }
 
-  private CodeableConcept getPlanJurisdictions(Location location) {
-    CodeableConcept codeableConcept = new CodeableConcept();
-    codeableConcept.setId(location.getIdentifier().toString());
-    codeableConcept.setText(location.getName());
-    return codeableConcept;
-  }
 
-  private PlanDefinitionGoalComponent getPlanDefinitionGoalComponent(Goal goal) {
-    PlanDefinitionGoalComponent goal1 = new PlanDefinitionGoalComponent();
-    goal1.setId(goal.getIdentifier().toString());
-
-    CodeableConcept goalDescription = new CodeableConcept();
-    goalDescription.setText(goal.getDescription());
-    goal1.setDescription(goalDescription);
-
-    CodeableConcept goalPriority = new CodeableConcept();
-    goalPriority.setText(goal.getPriority().toString());
-    goal1.setPriority(goalPriority);
-
-    goal.getActions().stream().flatMap(action -> action.getConditions().stream())
-        .flatMap(condition -> condition.getTargets().stream())
-        .map(this::getPlanDefinitionGoalTargetComponent).forEach(goal1::addTarget);
-    return goal1;
-  }
-
-  private PlanDefinitionGoalTargetComponent getPlanDefinitionGoalTargetComponent(Target target) {
-    PlanDefinitionGoalTargetComponent goalTargetComponent = new PlanDefinitionGoalTargetComponent();
-
-    CodeableConcept goalTargetMeasure = new CodeableConcept();
-    goalTargetMeasure.setText(target.getMeasure());
-    goalTargetComponent.setMeasure(goalTargetMeasure);
-
-    Duration goalTargetDuration = new Duration();
-    DecimalType duration = new DecimalType();
-    duration.setValue(target.getDue().toEpochDay() - LocalDate.now().toEpochDay());
-    StringType unitString = new StringType();
-    unitString.setValue("days");
-    goalTargetDuration.setProperty("value", duration);
-    goalTargetDuration.setProperty("unit", unitString);
-    goalTargetComponent.setDue(goalTargetDuration);
-
-    Quantity targetQuantity = new Quantity();
-    DecimalType detailValue = new DecimalType();
-    detailValue.setValue(target.getValue());
-    StringType comparator = new StringType();
-    comparator.setValue(target.getComparator());
-    StringType detailUnitString = new StringType();
-    detailUnitString.setValue(target.getUnit().toString());
-    targetQuantity.setProperty("unit", detailUnitString);
-    targetQuantity.setProperty("comparator", comparator);
-    targetQuantity.setProperty("value", detailValue);
-    goalTargetComponent.setDetail(targetQuantity);
-
-    return goalTargetComponent;
-  }
 }
---
+++ src/main/java/com/revealprecision/revealserver/fhir/util/FhirPlanUtil.java
@@ -0,0 +1,127 @@
+package com.revealprecision.revealserver.fhir.util;
+
+import com.revealprecision.revealserver.persistence.domain.Action;
+import com.revealprecision.revealserver.persistence.domain.Goal;
+import com.revealprecision.revealserver.persistence.domain.Location;
+import com.revealprecision.revealserver.persistence.domain.Target;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.DecimalType;
+import org.hl7.fhir.r4.model.Duration;
+import org.hl7.fhir.r4.model.Enumeration;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Period;
+import org.hl7.fhir.r4.model.PlanDefinition;
+import org.hl7.fhir.r4.model.PlanDefinition.PlanDefinitionActionComponent;
+import org.hl7.fhir.r4.model.PlanDefinition.PlanDefinitionGoalComponent;
+import org.hl7.fhir.r4.model.PlanDefinition.PlanDefinitionGoalTargetComponent;
+import org.hl7.fhir.r4.model.Quantity;
+import org.hl7.fhir.r4.model.StringType;
+import org.hl7.fhir.r4.model.TriggerDefinition;
+import org.hl7.fhir.r4.model.TriggerDefinition.TriggerType;
+import org.hl7.fhir.r4.model.TriggerDefinition.TriggerTypeEnumFactory;
+import org.hl7.fhir.r4.model.UriType;
+
+public class FhirPlanUtil {
+
+  public static PlanDefinition.PlanDefinitionActionComponent getActionComponent(Action action) {
+
+    PlanDefinitionActionComponent planDefinitionActionComponent = new PlanDefinitionActionComponent();
+
+    CodeableConcept subjectCodeableConcept = new CodeableConcept();
+    subjectCodeableConcept.setText("location");
+    planDefinitionActionComponent.setSubject(subjectCodeableConcept);
+
+    var coding = List.of(new CodeableConcept().setText(action.getIdentifier().toString()));
+
+    List<TriggerDefinition> triggerDefinitions = new ArrayList<>();
+    TriggerDefinition triggerDefinition = new TriggerDefinition();
+    TriggerTypeEnumFactory typeEnumFactory = new TriggerTypeEnumFactory();
+    Enumeration<TriggerType> enumeration = new Enumeration<>(typeEnumFactory);
+    enumeration.setValue(TriggerType.NAMEDEVENT);
+    triggerDefinition.setTypeElement(enumeration);
+    triggerDefinition.setNameElement(new StringType("plan-activation"));
+    triggerDefinitions.add(triggerDefinition);
+    planDefinitionActionComponent.setTrigger(triggerDefinitions);
+
+    planDefinitionActionComponent.setCode(coding);
+    planDefinitionActionComponent.setTitle(action.getTitle());
+
+    Period period = new Period();
+    period.setStart(Date.from(action.getTimingPeriodStart().atStartOfDay(
+        ZoneId.systemDefault()).toInstant()));
+
+    period.setEnd(Date.from(action.getTimingPeriodStart().atStartOfDay(
+        ZoneId.systemDefault()).toInstant()));
+
+    planDefinitionActionComponent.setTiming(period);
+
+    planDefinitionActionComponent.setDescription(action.getDescription());
+    planDefinitionActionComponent.setGoalId(List.of(new IdType(action.getGoal().getIdentifier().toString())));
+
+
+    planDefinitionActionComponent.setDefinition(new UriType().setValue(action.getForm().getIdentifier().toString()));
+
+    return planDefinitionActionComponent;
+  }
+
+  public static CodeableConcept getPlanJurisdictions(Location location) {
+    CodeableConcept codeableConcept = new CodeableConcept();
+    codeableConcept.setId(location.getIdentifier().toString());
+    codeableConcept.setText(location.getName());
+    return codeableConcept;
+  }
+
+  public static PlanDefinitionGoalComponent getPlanDefinitionGoalComponent(Goal goal) {
+    PlanDefinitionGoalComponent goal1 = new PlanDefinitionGoalComponent();
+    goal1.setId(goal.getIdentifier().toString());
+
+    CodeableConcept goalDescription = new CodeableConcept();
+    goalDescription.setText(goal.getDescription());
+    goal1.setDescription(goalDescription);
+
+    CodeableConcept goalPriority = new CodeableConcept();
+    goalPriority.setText(goal.getPriority().toString());
+    goal1.setPriority(goalPriority);
+
+    goal.getActions().stream().flatMap(action -> action.getConditions().stream())
+        .flatMap(condition -> condition.getTargets().stream())
+        .map(FhirPlanUtil::getPlanDefinitionGoalTargetComponent).forEach(goal1::addTarget);
+    return goal1;
+  }
+
+  public static PlanDefinitionGoalTargetComponent getPlanDefinitionGoalTargetComponent(Target target) {
+    PlanDefinitionGoalTargetComponent goalTargetComponent = new PlanDefinitionGoalTargetComponent();
+
+    CodeableConcept goalTargetMeasure = new CodeableConcept();
+    goalTargetMeasure.setText(target.getMeasure());
+    goalTargetComponent.setMeasure(goalTargetMeasure);
+
+    Duration goalTargetDuration = new Duration();
+    DecimalType duration = new DecimalType();
+    duration.setValue(target.getDue().toEpochDay() - LocalDate.now().toEpochDay());
+    StringType unitString = new StringType();
+    unitString.setValue("days");
+    goalTargetDuration.setProperty("value", duration);
+    goalTargetDuration.setProperty("unit", unitString);
+    goalTargetComponent.setDue(goalTargetDuration);
+
+    Quantity targetQuantity = new Quantity();
+    DecimalType detailValue = new DecimalType();
+    detailValue.setValue(target.getValue());
+    StringType comparator = new StringType();
+    comparator.setValue(target.getComparator());
+    StringType detailUnitString = new StringType();
+    detailUnitString.setValue(target.getUnit().toString());
+    targetQuantity.setProperty("unit", detailUnitString);
+    targetQuantity.setProperty("comparator", comparator);
+    targetQuantity.setProperty("value", detailValue);
+    goalTargetComponent.setDetail(targetQuantity);
+
+    return goalTargetComponent;
+  }
+}
