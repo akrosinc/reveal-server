2347613c4649407abccf6ae27488e6162bb09229
Milan Katic <milan.katic@vegait.rs>
2022-05-05 13:14:27 +0200

    bug fixing null children

--- src/main/java/com/revealprecision/revealserver/api/v1/dto/factory/LocationHierarchyResponseFactory.java
+++ src/main/java/com/revealprecision/revealserver/api/v1/dto/factory/LocationHierarchyResponseFactory.java
@@ -111,7 +111,9 @@ public class LocationHierarchyResponseFactory {
         .build();
     var geoTreeResponse = GeoTreeResponse.builder().identifier(node.getId())
        .geometry(includeGeometry ? node.getNode().getGeometry() : null)
-        .properties(locationPropertyResponse).build();
+        .properties(locationPropertyResponse)
+        .children(new ArrayList<>())
+        .build();
     List<GeoTreeResponse> children = new ArrayList<>();
     if (node.getChildren() != null) {
       for (Map.Entry<UUID, TreeNode<UUID, Location>> childEntry : node.getChildren().entrySet()) {
