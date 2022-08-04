1c2e84f191e35f57693d4ce31ddd3107f15375f7
trevlenb2 <trevlenb2@gmail.com>
2022-08-04 14:43:33 +0200

    metadata import file handling fix

--- src/main/java/com/revealprecision/revealserver/props/KafkaProperties.java
+++ src/main/java/com/revealprecision/revealserver/props/KafkaProperties.java
@@ -22,7 +22,7 @@ public class KafkaProperties {
 
   private Map<String, String> topicMap = new HashMap<>();
   private Map<String, Boolean> resetTopicMap = new HashMap<>();
-  private Map<String, Integer> topicConfigRetention = new HashMap<>();
+  private Map<String, String> topicConfigRetention = new HashMap<>();
 
   private List<String> storeNames = new ArrayList<>();
 
