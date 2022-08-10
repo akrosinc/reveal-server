1b6d0f71ecf9fd659b09c9a48d686ea178f45673
Milan Katic <milan.katic@vegait.rs>
2022-08-10 08:57:52 +0200

    handling elasticsearch geoshape errors

--- src/main/java/com/revealprecision/revealserver/batch/writer/LocationWriter.java
+++ src/main/java/com/revealprecision/revealserver/batch/writer/LocationWriter.java
@@ -1,20 +1,48 @@
 package com.revealprecision.revealserver.batch.writer;
 
+import com.revealprecision.revealserver.enums.EntityStatus;
 import com.revealprecision.revealserver.persistence.domain.Location;
+import com.revealprecision.revealserver.persistence.domain.LocationBulk;
+import com.revealprecision.revealserver.persistence.domain.LocationBulkException;
 import com.revealprecision.revealserver.persistence.es.LocationElastic;
+import com.revealprecision.revealserver.persistence.repository.LocationBulkExceptionRepository;
+import com.revealprecision.revealserver.persistence.repository.LocationBulkRepository;
 import com.revealprecision.revealserver.persistence.repository.LocationElasticRepository;
 import com.revealprecision.revealserver.persistence.repository.LocationRepository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.elasticsearch.BulkFailureException;
 
+@StepScope
+@Slf4j
 public class LocationWriter implements ItemWriter<Location> {
 
   @Autowired
   private LocationElasticRepository locationElasticRepository;
   @Autowired
   private LocationRepository locationRepository;
+  @Autowired
+  private LocationBulkRepository locationBulkRepository;
+  @Autowired
+  private LocationBulkExceptionRepository locationBulkExceptionRepository;
+
+  @Value("#{jobParameters['locationBulkId']}")
+  private String locationBulkId;
+  private LocationBulk locationBulk;
+
+  @BeforeStep
+  private void setUpData() {
+    locationBulk = locationBulkRepository.getById(UUID.fromString(locationBulkId));
+  }
 
   @Override
   public void write(List<? extends Location> items) throws Exception {
@@ -30,6 +58,30 @@ public class LocationWriter implements ItemWriter<Location> {
       locations.add(loc);
     });
 
-    locationElasticRepository.saveAll(locations);
+    try {
+      locationElasticRepository.saveAll(locations);
+    } catch (BulkFailureException e) {
+      System.out.println(e.getMessage());
+
+      e.getFailedDocuments().values().forEach(err -> log.error("Elasticsearch import location({}) error: {}", err));
+      Set<UUID> failedLocations = e.getFailedDocuments().keySet().stream().map(UUID::fromString)
+          .collect(
+              Collectors.toSet());
+      Set<String> failedLocationNames = items.stream()
+          .filter(el -> failedLocations.contains(el.getIdentifier()))
+          .map(Location::getName)
+          .collect(Collectors.toSet());
+      locationRepository.deleteFailedLocations(failedLocations);
+
+      List<LocationBulkException> exceptions = new ArrayList<>();
+      for (String name : failedLocationNames) {
+        LocationBulkException locationBulkException = LocationBulkException.builder().message("GeoJSON is formatted incorrectly")
+            .locationBulk(locationBulk).name(name).build();
+        locationBulkException.setEntityStatus(EntityStatus.ACTIVE);
+        exceptions.add(locationBulkException);
+      }
+      locationBulkExceptionRepository.saveAll(exceptions);
+    }
+
   }
 }
--- src/main/java/com/revealprecision/revealserver/persistence/repository/LocationRepository.java
+++ src/main/java/com/revealprecision/revealserver/persistence/repository/LocationRepository.java
@@ -5,10 +5,12 @@ import com.revealprecision.revealserver.persistence.projection.LocationCoordinat
 import com.revealprecision.revealserver.persistence.projection.PlanLocationDetails;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -88,4 +90,8 @@ public interface LocationRepository extends JpaRepository<Location, UUID> {
           + "(l.identifier, l.type, l.name, l.status, l.externalId, l.geographicLevel, l.locationBulk)"
           + " from Location l where l.identifier = :identifier")
   Optional<Location> findByIdentifierWithoutGeoJson(UUID identifier);
+
+  @Modifying
+  @Query(value = "delete from Location l where l.identifier in :identifiers")
+  void deleteFailedLocations(@Param("identifiers") Set<UUID> identifiers);
 }
