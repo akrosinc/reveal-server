081abb36626243e657569ecb811f6ef3289a22bd
trevlenb2 <trevlenb2@gmail.com>
2023-07-03 14:45:34 +0200

    kenya echis

--- src/main/resources/db/migration/V0_0_141__create_aggregation_staging.sql
+++ src/main/resources/db/migration/V0_0_141__create_aggregation_staging.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS aggregation_staging
     id SERIAL,
     location_identifier UUID,
     hierarchy_identifier UUID,
+    node_order character varying,
     PRIMARY KEY (id)
 );
+
