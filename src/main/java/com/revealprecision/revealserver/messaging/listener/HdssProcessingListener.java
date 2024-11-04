68976cabd9fc52b2de467e0bb492a585f0956a91
trevlenb2 <trevlenb2@gmail.com>
2024-11-04 23:03:19 +0200

    plan target debugging

--- src/main/java/com/revealprecision/revealserver/messaging/listener/HdssProcessingListener.java
+++ src/main/java/com/revealprecision/revealserver/messaging/listener/HdssProcessingListener.java
@@ -168,6 +168,13 @@ public class HdssProcessingListener extends Listener {
           });
 
           UUID planIdentifier1 = eventTrackerMessage.getPlanIdentifier();
+          log.debug("submitting plan {}", planIdentifier1);
+          if (hdssProperties.getTarget()!=null){
+            hdssProperties.getTarget().entrySet().stream().forEach(entry->{
+              log.debug("current plan targets {} {} ", entry.getKey(),entry.getValue());
+            });
+
+          }
           if (planIdentifier1 != null && hdssProperties.getTarget() != null) {
 
             UUID targetPlan = hdssProperties.getTarget().get(planIdentifier1);
