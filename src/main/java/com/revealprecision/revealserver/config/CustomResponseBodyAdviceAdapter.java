48df9d71b194cb699c1b03a117fd9bfa0bdda7a5
trevlenb2 <trevlenb2@gmail.com>
2025-01-02 16:24:08 +0200

    ef mali logging

--- src/main/java/com/revealprecision/revealserver/config/CustomResponseBodyAdviceAdapter.java
+++ src/main/java/com/revealprecision/revealserver/config/CustomResponseBodyAdviceAdapter.java
@@ -85,9 +85,9 @@ public class CustomResponseBodyAdviceAdapter implements ResponseBodyAdvice<Objec
               servletRequest.getRequestURL().toString() + (servletRequest.getQueryString() != null ?
                   "?"
                       + servletRequest.getQueryString() : ""), null, o, tracer.currentSpan(),
-              servletRequest.getMethod(), String.valueOf(
+              servletRequest.getMethod(), httpLoggingProperties.isLogPostBody()?String.valueOf(
                   ((ServletServerHttpResponse) serverHttpResponse).getServletResponse()
-                      .getStatus()),
+                      .getStatus()):null,
               headers, triggerTime, LocalDateTime.now(), username,
               jwtKid);
         }
--- src/main/java/com/revealprecision/revealserver/props/HttpLoggingProperties.java
+++ src/main/java/com/revealprecision/revealserver/props/HttpLoggingProperties.java
@@ -23,6 +23,8 @@ public class HttpLoggingProperties {
 
   private  Integer logLength = 2000;
 
+  private boolean logPostBody = false;
+
   private List<String> excludedPaths = List.of("/actuator/");
 
   private boolean shouldHideLocationResponses = true;
