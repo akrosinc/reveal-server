28d6623685108de406d5b7d856041d5e2e63f458
Bozana Ruljic <bozana.ruljic@vegait.rs>
2025-02-12 17:18:36 +0100

    fix: show only average tags

--- src/main/java/com/revealprecision/revealserver/api/v1/controller/EntityTagController.java
+++ src/main/java/com/revealprecision/revealserver/api/v1/controller/EntityTagController.java
@@ -200,7 +200,7 @@ public class EntityTagController {
     public ResponseEntity<TagResponse> getEntityTagsForDefaultHierarchy() {
         UUID defaultHierarchyId = locationHierarchyService.getDefaultHierarchy().getIdentifier();
         return ResponseEntity.status(HttpStatus.OK)
-                .body(entityTagService.getAllAggregateEntityTagsAssociatedToData(defaultHierarchyId.toString()));
+                .body(entityTagService.getAverageTags(defaultHierarchyId.toString()));
     }
 
     @AllArgsConstructor
