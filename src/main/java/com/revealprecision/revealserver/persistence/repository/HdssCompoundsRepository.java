2a076e585d8938b98d4894b01584331567824e7a
trevlenb2 <trevlenb2@gmail.com>
2025-07-22 20:31:18 +0200

    file for download of hdss data

--- src/main/java/com/revealprecision/revealserver/persistence/repository/HdssCompoundsRepository.java
+++ src/main/java/com/revealprecision/revealserver/persistence/repository/HdssCompoundsRepository.java
@@ -263,7 +263,7 @@ public interface HdssCompoundsRepository extends EntityGraphJpaRepository<HdssCo
       + "             left join users u on u.identifier = uo.user_identifier\n"
       + "    WHERE gl.name = lh.node_order[array_position(lh.node_order, pgl.name) - 1]\n"
       + "      and u.username = :username\n"
-      + ")"
+      + "))"
       + "  and hc.server_version > :serverVersion "
       + " LIMIT 10\n",nativeQuery = true)
   List<HdssCompoundHouseholdIndividualProjection> getAllCompoundsForUserAssignmentAndServerVersionV2(String username, long serverVersion);
