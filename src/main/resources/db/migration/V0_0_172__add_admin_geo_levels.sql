27e8c258e566ed18a86fa6980051ef22718ebb6c
trevlenb2 <trevlenb2@gmail.com>
2025-09-28 19:58:10 +0200

    fixing flyway scripts

--- src/main/resources/db/migration/V0_0_172__add_admin_geo_levels.sql
+++ src/main/resources/db/migration/V0_0_172__add_admin_geo_levels.sql
@@ -1,2 +1,50 @@
-ALTER TABLE IF EXISTS dataset
-ADD COLUMN IF NOT EXISTS border_color VARCHAR(7) NOT NULL;
+INSERT INTO geographic_level(identifier, name, title, entity_status, created_by, created_datetime,
+                             modified_by, modified_datetime)
+VALUES (uuid_generate_v4(), 'admin0', 'Admin0', 'ACTIVE', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00') ON CONFLICT DO NOTHING ;
+INSERT INTO geographic_level(identifier, name, title, entity_status, created_by, created_datetime,
+                             modified_by, modified_datetime)
+VALUES (uuid_generate_v4(), 'admin1', 'Admin1', 'ACTIVE', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00') ON CONFLICT DO NOTHING ;
+INSERT INTO geographic_level(identifier, name, title, entity_status, created_by, created_datetime,
+                             modified_by, modified_datetime)
+VALUES (uuid_generate_v4(), 'admin2', 'Admin2', 'ACTIVE', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00') ON CONFLICT DO NOTHING ;
+INSERT INTO geographic_level(identifier, name, title, entity_status, created_by, created_datetime,
+                             modified_by, modified_datetime)
+VALUES (uuid_generate_v4(), 'admin3', 'Admin3', 'ACTIVE', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00') ON CONFLICT DO NOTHING ;
+INSERT INTO geographic_level(identifier, name, title, entity_status, created_by, created_datetime,
+                             modified_by, modified_datetime)
+VALUES (uuid_generate_v4(), 'admin4', 'Admin4', 'ACTIVE', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00') ON CONFLICT DO NOTHING ;
+INSERT INTO geographic_level(identifier, name, title, entity_status, created_by, created_datetime,
+                             modified_by, modified_datetime)
+VALUES (uuid_generate_v4(), 'admin5', 'Admin5', 'ACTIVE', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00') ON CONFLICT DO NOTHING ;
+INSERT INTO geographic_level(identifier, name, title, entity_status, created_by, created_datetime,
+                             modified_by, modified_datetime)
+VALUES (uuid_generate_v4(), 'admin6', 'Admin6', 'ACTIVE', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00') ON CONFLICT DO NOTHING ;
+INSERT INTO geographic_level(identifier, name, title, entity_status, created_by, created_datetime,
+                             modified_by, modified_datetime)
+VALUES (uuid_generate_v4(), 'admin7', 'Admin7', 'ACTIVE', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00') ON CONFLICT DO NOTHING ;
+INSERT INTO geographic_level(identifier, name, title, entity_status, created_by, created_datetime,
+                             modified_by, modified_datetime)
+VALUES (uuid_generate_v4(), 'admin8', 'Admin8', 'ACTIVE', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00') ON CONFLICT DO NOTHING ;
+INSERT INTO geographic_level(identifier, name, title, entity_status, created_by, created_datetime,
+                             modified_by, modified_datetime)
+VALUES (uuid_generate_v4(), 'admin9', 'Admin9', 'ACTIVE', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00', '71fca736-c156-40bc-9de5-3ae04981fbc9',
+        '2022-01-12 13:54:22.106221+00') ON CONFLICT DO NOTHING ;
