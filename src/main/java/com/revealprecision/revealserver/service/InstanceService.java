57683cb1d170a4f3cb2f56e57b76481afdd65771
haiderali22 <58180942+haiderali22@users.noreply.github.com>
2026-03-11 01:53:49 +0500

    added aud tables

--- src/main/java/com/revealprecision/revealserver/service/InstanceService.java
+++ src/main/java/com/revealprecision/revealserver/service/InstanceService.java
@@ -27,7 +27,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
