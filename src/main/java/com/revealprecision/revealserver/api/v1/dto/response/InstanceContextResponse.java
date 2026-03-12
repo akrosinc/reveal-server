1793ef90b5f6f54db5f372660de4b28ba69b780d
haiderali22 <58180942+haiderali22@users.noreply.github.com>
2026-03-12 14:02:04 +0500

    added new class InstanceContextResponse

---
+++ src/main/java/com/revealprecision/revealserver/api/v1/dto/response/InstanceContextResponse.java
@@ -0,0 +1,19 @@
+package com.revealprecision.revealserver.api.v1.dto.response;
+
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InstanceContextResponse {
+  private IdentifierNameResponse selectedInstance;
+  List<String> roles;
+}
