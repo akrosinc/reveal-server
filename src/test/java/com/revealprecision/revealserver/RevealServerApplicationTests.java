136e82db249dfb6a8aaf2a70f1624c0cf92e0ead
haiderali22 <58180942+haiderali22@users.noreply.github.com>
2026-03-16 11:27:55 +0500

    fixed organizationType  in v1/groupmanagement api

--- src/test/java/com/revealprecision/revealserver/RevealServerApplicationTests.java
+++ src/test/java/com/revealprecision/revealserver/RevealServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.revealprecision.revealserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RevealServerApplicationTests {
 
     @Test
