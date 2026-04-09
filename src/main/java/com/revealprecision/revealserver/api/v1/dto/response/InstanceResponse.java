7f4a01481768aa4b661f397710aa66d7a84676cf
haiderali22 <58180942+haiderali22@users.noreply.github.com>
2026-04-09 15:44:33 +0500

    added complex-tags apis

--- src/main/java/com/revealprecision/revealserver/api/v1/dto/response/InstanceResponse.java
+++ src/main/java/com/revealprecision/revealserver/api/v1/dto/response/InstanceResponse.java
@@ -27,4 +27,6 @@ public class InstanceResponse {
   private List<LocationHierarchyResponse> locationHierarchy;
 
   private List<IdentifierNameResponse> datasets;
+
+  private List<IdNameResponse> complexTags;
 }
