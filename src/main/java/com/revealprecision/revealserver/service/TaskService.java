3dd1d64539c767ea2e23d4c802ab5ad50e43218c
trevlenb2 <trevlenb2@gmail.com>
2026-08-25 20:41:38 +0200

    test android templates

--- src/main/java/com/revealprecision/revealserver/service/TaskService.java
+++ src/main/java/com/revealprecision/revealserver/service/TaskService.java
@@ -236,7 +236,6 @@ public class TaskService {
                 ActionTitleEnum.SECONDARY_INDEX_CASE, ActionTitleEnum.INDEX_CASE_MEMBER,
                 ActionTitleEnum.SECONDARY_INDEX_CASE_MEMBER,ActionTitleEnum.RCD_MEMBER,
                 ActionTitleEnum.STRUCTURE_SURVEY,
-                ActionTitleEnum.ENROLMENT,ActionTitleEnum.ENROLMENT_STRUCTURE,
                 ActionTitleEnum.FOLLOWUP_STRUCTURE,ActionTitleEnum.FOLLOWUP,ActionTitleEnum.DESTRUCTION,
             ActionTitleEnum.RAPID_COVERAGE_STRUCTURE, ActionTitleEnum.RAPID_COVERAGE).map(
                 ActionTitleEnum::getActionTitle).collect(
